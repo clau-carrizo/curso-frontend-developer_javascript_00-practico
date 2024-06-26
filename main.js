8687fd903c5514c3ccccdbc3a1ca5c3f6c5c89ff
Clau Carrizo <claucarrizo2309@gmail.com>
2024-06-25 21:07:15 -0500

    integración de menú desktop y mobile, activar y desactivar menús con toggle en Js

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 const menuEmail = document.querySelector('.navbar-email');
 const DesktopMenu = document.querySelector('.desktop-menu');
+const burgerIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+burgerIcon.addEventListener('click', toggleMobileMenu);
+
 
 function toggleDesktopMenu(){
   DesktopMenu.classList.toggle('inactive');
 }
 
-console.log('JS Funcionando')
+function toggleMobileMenu(){
+  mobileMenu.classList.toggle('inactive');
+}
